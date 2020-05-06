184533332010d9ed9b1bd4c50d2e7da66e8246ba
Sam Dean <sam.dean@substrakt.com>
2020-05-06 21:06:13 +0100

    ADD: ENV file

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, send_from_directory
 from flask_restful import Resource, Api, reqparse
+from dotenv import load_dotenv
 import gspread
 import os
 
+load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 
 
+google_creds = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+
+gc = gspread.service_account(filename=google_creds)
+sh = gc.open("Museums")
+worksheet = sh.sheet1
+
 class MuseumsList(Resource):
     def get(self):
-        gc = gspread.service_account()
-        sh = gc.open("Museums")
-        worksheet = sh.sheet1
         museums = worksheet.get_all_records()
         return museums
 
@@ -20,9 +26,6 @@ api.add_resource(MuseumsList, '/')
 
 class Museums(Resource):
     def get(self, museum_id):
-        gc = gspread.service_account()
-        sh = gc.open("Museums")
-        worksheet = sh.sheet1
         values_list = worksheet.row_values(museum_id)
         return(values_list)
 
