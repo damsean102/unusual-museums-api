af24241fa47a8fcc9d8ac12ef73c38b92ff62ee3
Sam Dean <sam.dean@substrakt.com>
2020-05-06 21:47:01 +0100

    UPDATE: DRY the code

--- app.py
+++ app.py
@@ -8,17 +8,16 @@ load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 
-
+#Load env Fil
 google_creds = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-
 
 gc = gspread.service_account(filename=google_creds)
 sh = gc.open("Museums")
 worksheet = sh.sheet1
+museums = worksheet.get_all_records()
 
 class MuseumsList(Resource):
     def get(self):
-        museums = worksheet.get_all_records()
         return museums
 
 api.add_resource(MuseumsList, '/')
@@ -26,8 +25,7 @@ api.add_resource(MuseumsList, '/')
 
 class Museums(Resource):
     def get(self, museum_id):
-        values_list = worksheet.row_values(museum_id)
-        return(values_list)
+        return museums[int(museum_id)]
 
 api.add_resource(Museums, '/<museum_id>')
 
